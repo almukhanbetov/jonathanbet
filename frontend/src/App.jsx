b99d1c64edd8007222e08056bb68446dda49ac64
Almukhanbetov <almuko.m@gmail.com>
2025-05-13 14:38:42 +0500

    .env VITE_API_URL=/api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("http://localhost:8081/structured");
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/structured`);
         setData(res.data);
         const firstSport = Object.keys(res.data)[0];
         setSelectedSport(firstSport);
